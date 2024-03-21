e15d87b5f0cee88ce3ea406db8c8d02a2ce7362e
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-03-21 23:40:42 +0200

    feat: implemented struct ComputeHandler with its method Compute and added support for command line arguments

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,32 +1,68 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	lab2 "github.com/our-mind-game/kpi-architecture-lab2"
+	"io"
 	"os"
+	"strings"
 )
 
 var (
-	inputExpression = flag.String("e", "", "Expression to compute")
-	// TODO: Add other flags support for input and output configuration.
+	inputExpression = flag.String("e", "", "Expression to convert")
+	inputFile       = flag.String("f", "", "Input expression from file")
+	outputFile      = flag.String("o", "", "File to store the result of converting the expression")
 )
 
 func main() {
 	flag.Parse()
+	checkFlags(*inputExpression, *inputFile)
 
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
+	var input io.Reader
+	var output io.Writer
 
-	res, err := lab2.PostfixToPrefix("7 35 + 90 * *")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+	if *inputExpression != "" {
+		input = strings.NewReader(*inputExpression)
+	} else {
+		file, err := os.Open(*inputFile)
+		exitIfError(err)
+		defer file.Close()
+		input = file
+	}
+
+	if *outputFile != "" {
+		file, err := os.Create(*outputFile)
+		exitIfError(err)
+		defer file.Close()
+		output = file
+	} else {
+		output = os.Stdout
+	}
+
+	handler := &lab2.ComputeHandler{
+		Input:  input,
+		Output: output,
+	}
+	err := handler.Compute()
+	exitIfError(err)
+}
+
+func checkFlags(expressionFlag string, fileFlag string) {
+	if expressionFlag == "" && fileFlag == "" {
+		err := errors.New("unspecified input")
+		exitIfError(err)
+	}
+	if expressionFlag != "" && fileFlag != "" {
+		err := errors.New("both -e and -f flags cannot be specified")
+		exitIfError(err)
+	}
+}
+
+func exitIfError(error error) {
+	if error != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", error)
 		os.Exit(1)
 	}
-	fmt.Println(res)
 }
--- handler.go
+++ handler.go
@@ -1,12 +1,32 @@
 package lab2
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+)
+
 // ComputeHandler should be constructed with input io.Reader and output io.Writer.
 // Its Compute() method should read the expression from input and write the computed result to the output.
 type ComputeHandler struct {
-	// TODO: Add necessary fields.
+	Input  io.Reader
+	Output io.Writer
 }
 
 func (ch *ComputeHandler) Compute() error {
-	// TODO: Implement.
+	scanner := bufio.NewScanner(ch.Input)
+	scanner.Scan()
+	expression := scanner.Text()
+
+	res, err := PostfixToPrefix(expression)
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintln(ch.Output, res)
+	if err != nil {
+		return fmt.Errorf("failed to write output: %v", err)
+	}
+
 	return nil
 }
